961b8bfb88f48e6d8736ceed8634c7ef51fc450d
harold <haroldgonzales@yahoo.com>
2023-05-10 01:29:39 -0700

    Created the Header.js

---
+++ components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+import '../Header/Header.css';
+
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Harold Gonzales</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
