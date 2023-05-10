6f4f03bc7b33a03d498d0bb9c89334bffc357ea5
harold <haroldgonzales@yahoo.com>
2023-05-10 13:25:41 -0700

    Updates the Resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -11,9 +11,34 @@ const Resume = () => {
       </a>
       <h3>Proficiencies</h3>
       <ul>
-        <li>Proficiency 1</li>
-        {/* Add more proficiencies */}
+        <li>Front-end Development Proficiency:</li>
       </ul>
+      <ul>
+        <li>
+      Proficient in building interactive, user-friendly interfaces using JavaScript, React.js, and Redux.
+Strong understanding of front-end web technologies including HTML5, CSS3, and JavaScript ES6+.
+Experienced in responsive web design to ensure websites render well across various devices.
+Proficient in using modern development tools and methodologies, such as version control with Git, Agile methodologies, and test-driven development.
+Skilled in optimizing web performance and implementing SEO best practices.
+Experienced in working with RESTful APIs and GraphQL to integrate front-end applications with back-end services.
+      </li>
+      </ul>
+      <ul>
+        <li>Back-end Development Proficiency:</li>
+      </ul>
+      <ul>
+        <li>
+        Proficient in building scalable, secure server-side applications using Node.js and Express.js.
+Experienced in developing RESTful APIs and GraphQL APIs for front-end consumption.
+Strong understanding of relational databases and NoSQL databases, including experience with MySQL, PostgreSQL, and MongoDB.
+Proficient in authentication and authorization protocols, such as OAuth and JWT.
+Skilled in using Docker for containerization and Kubernetes for orchestration.
+Proficient in designing and implementing microservices architecture.
+Experienced in using modern development tools and methodologies, such as version control with Git, Agile methodologies, and test-driven development.
+      </li>
+      </ul>
+
+
     </section>
   );
 };
