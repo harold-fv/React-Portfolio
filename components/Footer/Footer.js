e644b969d0e9d354fa7f7b83402f392342096e4d
harold <haroldgonzales@yahoo.com>
2023-05-10 01:28:43 -0700

    Created the Footer.js and Footer.css

---
+++ components/Footer/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-links">
+        <a
+          href="https://github.com/harold-fv"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/harold-gonazales-24284a4b"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+        <a
+          href="https://stackoverflow.com/users"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Stack Overflow
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
