9b6301be1a80919330857869a09704eeb8d8fdad
harold <haroldgonzales@yahoo.com>
2023-05-10 20:53:40 -0700

    Updates the Index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// Imports the necessary React and ReactDOM libraries, as well as the main App component.
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Uses ReactDOM's render method to render the App component into the HTML element with the id 'root'.
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
