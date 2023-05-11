ea4f1e3f6c3190e0895f4d83beebaa6d86918a69
harold <haroldgonzales@yahoo.com>
2023-05-11 09:28:46 -0700

    Fixed the Cotact form to clear the forms onces submitted and send the data to the console.log

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact/Contact.css';
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(`Name: ${name}, Email: ${email}, Message: ${message}`);
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <section className="contact">
       <div className="contact-container">
         <h2>Contact</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" id="name" required />
+          <input type="text" id="name" value={name} required onChange={e => setName(e.target.value)} />
 
           <label htmlFor="email">Email</label>
           <input
             type="email"
             id="email"
+            value={email}
             pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
             required
+            onChange={e => setEmail(e.target.value)}
           />
 
           <label htmlFor="message">Message</label>
-          <textarea id="message" required></textarea>
+          <textarea id="message" value={message} required onChange={e => setMessage(e.target.value)}></textarea>
 
           <button type="submit">Submit</button>
         </form>
